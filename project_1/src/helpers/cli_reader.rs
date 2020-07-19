394edb67f8ecd6c56f4b942662a1367a3d47d7c3
DogeKun <36155578+DogeKun@users.noreply.github.com>
2020-07-19 15:44:35 +0300

    updated exchanged truncate with trim

--- project_1/src/helpers/cli_reader.rs
+++ project_1/src/helpers/cli_reader.rs
@@ -4,7 +4,6 @@ impl UserInput {
     pub fn read_line(error_msg: &str) -> String {
         let mut line = String::new();
         std::io::stdin().read_line(&mut line).expect(error_msg);
-        line.truncate(line.len() - 2);
-        return line;
+        return line.trim().to_string();
     }
 }
